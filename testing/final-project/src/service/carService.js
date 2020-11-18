564be2ce5a7ee72547b91a68467afd431f3ddabe
RafaDw <www.rafadraw@gmail.com>
2020-11-18 12:48:10 -0300

    07 - Projeto final - TDD & BDD - Parte 03

--- testing/final-project/src/service/carService.js
+++ testing/final-project/src/service/carService.js
@@ -1,8 +1,12 @@
 const BaseRepository = require('./../repository/base/baseRepository');
+const Tax = require('./../entities/tax');
+const Transaction = require('./../entities/transaction');
 
 class CarService {
 	constructor({cars}) {
 		this.carRepository = new BaseRepository({file: cars})
+		this.taxesBasedOnAge = Tax.taxBasedOnAge;
+		this. currencyFormat = new Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'})
 
 	}
 
@@ -23,6 +27,29 @@ class CarService {
 		const carId = this.choseRandomCar(carCategory);
 		return  await this.carRepository.find(carId);
 
+	}
+
+	calculateFinalPrice(customer, carCategory, numberOfDays) {
+		const { age } = customer;
+		const price  = carCategory.price;
+		const { then: tax } = this.taxesBasedOnAge.find(tax => age >= tax.from && age <= tax.to);
+		const finalPrice = ((tax * price) * (numberOfDays))
+
+		return this.currencyFormat.format(finalPrice);
+	}
+
+	async rent(customer, carCategory, numberOfDays) {
+		const car = await this.getAvailableCar(carCategory);
+		const finalPrice = await this.calculateFinalPrice(customer, carCategory, numberOfDays);
+
+		const today = new Date();
+		const options = { year: 'numeric', month: 'long', day: 'numeric' }
+
+		today.setDate(today.getDate() + numberOfDays);
+
+		const dueDate = today.toLocaleDateString('pt-br', options)
+
+		return new Transaction({customer, car, dueDate, amount: finalPrice});
 	}
 }
 
