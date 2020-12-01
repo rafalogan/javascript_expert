63d210a5de9047f03cb43f8b915a64c837e6b911
RafaDw <www.rafadraw@gmail.com>
2020-12-01 14:18:35 -0300

    Projeto Parte 03 - Como testar, Armazenamento de dados e Atualização de tabela

--- jsFundamentals-2/project-modulo/src/index.js
+++ jsFundamentals-2/project-modulo/src/index.js
@@ -1,8 +1,14 @@
 import database from './../database.json';
 import TerminalController from "./terminalController.js";
 import Person from "./person.js";
+import {save} from "./repository.js";
 
-const DEFAULT_LANG = 'pt-br';
+
+
+const DEFAULT_LANG = 'pt-BR';
+// const DEFAULT_LANG = 'es';
+// const DEFAULT_LANG = 'en';
+// const DEFAULT_LANG = 'rus';
 const STOP_TERM = ':q';
 
 const terminalController = new TerminalController();
@@ -15,7 +21,10 @@ async function mainLoop() {
 		if (answer === STOP_TERM) return terminalController.closeTerminal();
 
 		const person = Person.genaeratInstanceFromString(answer);
-		console.log('person', person.formatted(DEFAULT_LANG));
+		terminalController.updateTable(person.formatted(DEFAULT_LANG));
+
+		await save(person);
+
 		return mainLoop();
 	} catch (error) {
 		console.error('Deu Ruim!', error);
