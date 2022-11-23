1b56b5b1f9b1aa3498f43493ac9814b1476a7768
RafaDraw <www.rafadraw@gmail.com>
2022-11-23 15:15:16 -0300

    fix interetors

--- 11-nodejs-streams/02-async-iterators/webapi.js
+++ 11-nodejs-streams/02-async-iterators/webapi.js
@@ -3,11 +3,12 @@ import { Readable } from 'stream';
 
 const api1 = (req, res) => {
 	let count = 0;
+    const maxItens = 99;
 	const readable = Readable({
 		read() {
 			const everySecond = (intervalContext) => {
-				if (count++ <= 5) {
-					return this.push(`My name is Rafa[${count}]`)
+				if (count++ <= maxItens) {
+					return this.push(JSON.stringify({ id: Date.now() + count, name: `Rafa-${count}`}) + '\n')
 				}
 				clearInterval(intervalContext)
 				this.push(null);
@@ -16,8 +17,29 @@ const api1 = (req, res) => {
 			setInterval(function () {everySecond(this)});
 		}
 	})
+
+    readable.pipe(res)
 };
-const api2 = (req, res) => {};
+
+const api2 = (req, res) => {
+	let count = 0;
+	const maxItens = 99;
+	const readable = Readable({
+		read() {
+			const everySecond = (intervalContext) => {
+				if (count++ <= maxItens) {
+					return this.push(JSON.stringify({ id: Date.now() + count, name: `Zézin-${count}`}) + '\n')
+				}
+				clearInterval(intervalContext)
+				this.push(null);
+			};
+
+			setInterval(function () {everySecond(this)});
+		}
+	})
+
+	readable.pipe(res)
+};
 
 http.createServer(api1).listen(3000, () => console.log('server running at 3000'))
-http.createServer(api2).listen(4000, () => console.log('server running at 3000'))
+http.createServer(api2).listen(4000, () => console.log('server running at 4000'))
